382e9ee31b9bbfbb501180c742e93fcdd9f2ea3f
NarciSource <jwch11@gmail.com>
2024-04-17 00:23:36 +0900

    fix: 초기 탭 수정

--- src/components/TransTabComp.jsx
+++ src/components/TransTabComp.jsx
@@ -3,13 +3,11 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 
 function TransTabComp() {
-    const currencies = ["USD", "CAD", "KRW", "HKD", "JPY", "CNY"];
-
     const sourceMoney = useSelector((state) => state.wallet.money); // observer
     const exchangeRate = useSelector((state) => state.wallet.exchangeRate);
     const targetCurrencies = useSelector((state) => state.wallet.targetCurrencies);
 
-    const [selectedCurrency, setSelectedCurrency] = useState(currencies[0]);
+    const [selectedCurrency, setSelectedCurrency] = useState(targetCurrencies[0]);
 
     return (
         <div>
