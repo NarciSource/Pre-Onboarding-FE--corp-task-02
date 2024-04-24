04622e4c7681007b480c732935b289f605b37f6a
NarciSource <jwch11@gmail.com>
2024-04-25 02:08:01 +0900

    refactor: 키 env로 관리

--- src/network/callExchange.js
+++ src/network/callExchange.js
@@ -1,12 +1,10 @@
-import keys from "../keys.json";
-
 export default async function callExchange({ base, symbols }) {
     const url = `https://api.apilayer.com/exchangerates_data/latest?symbols=${symbols.toString()}&base=${base}`;
     const requestOptions = {
         method: "GET",
         redirect: "follow",
         headers: {
-            apikey: keys.APILAYER_APIKEY,
+            apikey: process.env.APILAYER_APIKEY,
         },
     };
 
